e76b353209f23547ac3e95d9c13990ef17beb02e
MiniEggz <owenscook1@gmail.com>
2023-12-30 09:25:16 +0000

    Add remap for copying to clipboard in normal mode.

--- nvim/lua/owen/remap.lua
+++ nvim/lua/owen/remap.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<leader>pv', ':Ex<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
